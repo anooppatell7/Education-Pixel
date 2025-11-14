9da662fa1e4323052456642930efaac07f643652
Anoop Patel <anooppbh8@gmail.com>
2025-11-14 15:44:19 +0000

    Try fixing this error: `Console Error: Invalid DOM property `stop-color`

--- src/components/homepage/about.tsx
+++ src/components/homepage/about.tsx
@@ -12,7 +12,7 @@ import { cn } from "@/lib/utils";
 const IconTally = ({ className }: { className?: string }) => (
      <svg className={cn("h-10 w-10", className)} viewBox="0 123.306 595.279 595.279" xmlns="http://www.w3.org/2000/svg">
         <defs>
-            <radialGradient id="a" cx="-183.69" cy="328.972" r=".76" gradientTransform="matrix(545.6736 0 0 528.3113 100439.305 -173525.125)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#34364e" stop-opacity=".98"/><stop offset="1" stop-color="#0c0824"/></radialGradient>
+            <radialGradient id="a" cx="-183.69" cy="328.972" r=".76" gradientTransform="matrix(545.6736 0 0 528.3113 100439.305 -173525.125)" gradientUnits="userSpaceOnUse"><stop offset="0" stopColor="#34364e" stopOpacity=".98"/><stop offset="1" stopColor="#0c0824"/></radialGradient>
         </defs>
         <path d="M24.803 155.549h545.674v530.792H24.803V155.549z" fill="url(#a)"/>
         <path d="m401.318 450.958-26.291-18.106c0-8.929 4.464-15.13 30.756-28.772 38.941-18.851 51.095-36.957 51.095-63.497 0-39.685-30.26-61.016-71.186-61.016-21.579 0-40.182-4.465-50.847-10.665-1.736-.744-1.984-1.984-1.984-3.969v-36.461c0-2.48 1.24-3.225 2.977-1.984 15.626 10.17 33.484 14.634 49.854 14.634 19.595 0 27.78-8.185 27.78-19.347 0-8.929-5.705-16.866-30.757-29.764-35.221-16.866-49.854-33.98-49.854-62.504 0-31.997 25.052-58.536 68.457-58.536 21.331 0 36.213 3.225 44.398 6.945 1.984 1.24 2.48 3.224 2.48 4.96v33.98c0 1.984-1.24 3.225-3.721 2.48-10.913-6.943-27.035-11.16-43.157-11.16zm-213.309 29.516c5.705.496 10.17.496 20.091.496 29.021 0 56.304-10.169 56.304-49.606 0-31.5-19.595-47.375-52.583-47.375-9.921 0-19.347.496-23.812.744v95.741zM143.86 266.668c0-1.736 3.473-2.977 5.456-2.977 15.875-.744 39.438-1.24 63.993-1.24 68.705 0 95.492 37.701 95.492 85.82 0 63-45.638 90.036-101.693 90.036-9.425 0-12.649-.496-19.347-.496v95.245c0 1.984-.744 2.976-2.976 2.976h-37.949c-1.984 0-2.977-.744-2.977-2.976V266.668z" fill="#31c5f0"/>
@@ -28,17 +28,17 @@ const IconExcel = ({ className }: { className?: string }) => (
         <path d="M8 4C8 2.89543 8.89543 2 10 2H20V9H8V4Z" fill="#1D854F"/>
         <rect x="8" y="9" width="12" height="7" fill="#197B43"/>
         <rect x="8" y="16" width="12" height="7" fill="#1B5B38"/>
-        <path d="M8 12C8 10.3431 9.34315 9 11 9H17C18.6569 9 20 10.3431 20 12V24C20 25.6569 18.6569 27 17 27H8V12Z" fill="#000000" fill-opacity="0.3"/>
+        <path d="M8 12C8 10.3431 9.34315 9 11 9H17C18.6569 9 20 10.3431 20 12V24C20 25.6569 18.6569 27 17 27H8V12Z" fill="#000000" fillOpacity="0.3"/>
         <rect y="7" width="18" height="18" rx="2" fill="url(#paint1_linear_87_7712)"/>
         <path d="M13 21L10.1821 15.9L12.8763 11H10.677L9.01375 14.1286L7.37801 11H5.10997L7.81787 15.9L5 21H7.19931L8.97251 17.6857L10.732 21H13Z" fill="white"/>
         <defs>
         <linearGradient id="paint0_linear_87_7712" x1="8" y1="26.5" x2="32" y2="26.5" gradientUnits="userSpaceOnUse">
-        <stop stop-color="#163C27"/>
-        <stop offset="1" stop-color="#2A6043"/>
+        <stop stopColor="#163C27"/>
+        <stop offset="1" stopColor="#2A6043"/>
         </linearGradient>
         <linearGradient id="paint1_linear_87_7712" x1="0" y1="16" x2="18" y2="16" gradientUnits="userSpaceOnUse">
-        <stop stop-color="#185A30"/>
-        <stop offset="1" stop-color="#176F3D"/>
+        <stop stopColor="#185A30"/>
+        <stop offset="1" stopColor="#176F3D"/>
         </linearGradient>
         </defs>
     </svg>
@@ -50,29 +50,29 @@ const IconWord = ({ className }: { className?: string }) => (
         <path d="M8 23H32V28C32 29.1046 31.1046 30 30 30H10C8.89543 30 8 29.1046 8 28V23Z" fill="url(#paint1_linear_87_7724)"/>
         <rect x="8" y="16" width="24" height="7" fill="url(#paint2_linear_87_7724)"/>
         <rect x="8" y="9" width="24" height="7" fill="url(#paint3_linear_87_7724)"/>
-        <path d="M8 12C8 10.3431 9.34315 9 11 9H17C18.6569 9 20 10.3431 20 12V24C20 25.6569 18.6569 27 17 27H8V12Z" fill="#000000" fill-opacity="0.3"/>
+        <path d="M8 12C8 10.3431 9.34315 9 11 9H17C18.6569 9 20 10.3431 20 12V24C20 25.6569 18.6569 27 17 27H8V12Z" fill="#000000" fillOpacity="0.3"/>
         <rect y="7" width="18" height="18" rx="2" fill="url(#paint4_linear_87_7724)"/>
         <path d="M15 11.0142H13.0523L11.5229 17.539L9.84967 11H8.20261L6.51634 17.539L5 11.0142H3L5.60131 21H7.3268L9 14.6879L10.6732 21H12.3987L15 11.0142Z" fill="white"/>
         <defs>
         <linearGradient id="paint0_linear_87_7724" x1="8" y1="6.66667" x2="32" y2="6.66667" gradientUnits="userSpaceOnUse">
-        <stop stop-color="#2B78B1"/>
-        <stop offset="1" stop-color="#338ACD"/>
+        <stop stopColor="#2B78B1"/>
+        <stop offset="1" stopColor="#338ACD"/>
         </linearGradient>
         <linearGradient id="paint1_linear_87_7724" x1="8" y1="27.375" x2="32" y2="27.375" gradientUnits="userSpaceOnUse">
-        <stop stop-color="#1B366F"/>
-        <stop offset="1" stop-color="#2657B0"/>
+        <stop stopColor="#1B366F"/>
+        <stop offset="1" stopColor="#2657B0"/>
         </linearGradient>
         <linearGradient id="paint2_linear_87_7724" x1="18.5" y1="20" x2="32" y2="20" gradientUnits="userSpaceOnUse">
-        <stop stop-color="#20478B"/>
-        <stop offset="1" stop-color="#2D6FD1"/>
+        <stop stopColor="#20478B"/>
+        <stop offset="1" stopColor="#2D6FD1"/>
         </linearGradient>
         <linearGradient id="paint3_linear_87_7724" x1="18.5" y1="13" x2="32" y2="13" gradientUnits="userSpaceOnUse">
-        <stop stop-color="#215295"/>
-        <stop offset="1" stop-color="#2E84D3"/>
+        <stop stopColor="#215295"/>
+        <stop offset="1" stopColor="#2E84D3"/>
         </linearGradient>
         <linearGradient id="paint4_linear_87_7724" x1="3.31137e-08" y1="17" x2="19" y2="17" gradientUnits="userSpaceOnUse">
-        <stop stop-color="#223E74"/>
-        <stop offset="1" stop-color="#215091"/>
+        <stop stopColor="#223E74"/>
+        <stop offset="1" stopColor="#215091"/>
         </linearGradient>
         </defs>
     </svg>
@@ -168,3 +168,5 @@ export default function About() {
     </section>
   );
 }
+
+    
