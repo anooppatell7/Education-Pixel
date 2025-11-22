2f14745565c96bb2b8014c46f257e70dee5750b2
Anoop Patel <anooppbh8@gmail.com>
2025-11-22 07:18:43 +0000

    ab sahi hai lekin look at the certificate its looking empty and there is

--- src/components/certificate-template.tsx
+++ src/components/certificate-template.tsx
@@ -21,141 +21,111 @@ export default function CertificateTemplate(data: CertificateData) {
         page: {
             width: '1123px',
             height: '794px',
-            padding: '75px',
+            padding: '40px',
             boxSizing: 'border-box' as 'border-box',
             backgroundColor: '#ffffff',
             fontFamily: 'Helvetica, Arial, sans-serif',
             color: '#333',
             position: 'relative' as 'relative',
-            display: 'flex',
-            flexDirection: 'column' as 'column',
+        },
+        border: {
+            position: 'absolute' as 'absolute',
+            top: '20px',
+            left: '20px',
+            right: '20px',
+            bottom: '20px',
+            border: '5px double #30475E',
+            boxSizing: 'border-box' as 'border-box',
         },
         watermark: {
             position: 'absolute' as 'absolute',
             top: '50%',
             left: '50%',
             transform: 'translate(-50%, -50%)',
-            opacity: 0.08,
-            width: '450px',
-            height: '450px',
-            zIndex: 1,
+            opacity: 0.07,
+            width: '400px',
+            height: '400px',
         },
         content: {
+            textAlign: 'center' as 'center',
             position: 'relative' as 'relative',
             zIndex: 2,
-            textAlign: 'center' as 'center',
-            display: 'flex',
-            flexDirection: 'column' as 'column',
-            justifyContent: 'center',
-            height: '100%',
-        },
-        border: {
-            position: 'absolute' as 'absolute',
-            top: '38px',
-            left: '38px',
-            right: '38px',
-            bottom: '38px',
-            border: '2px solid #30475E',
-            boxSizing: 'border-box' as 'border-box',
-            padding: '19px',
-            borderStyle: 'double' as 'double',
-            borderColor: '#30475E',
-            borderWidth: '5px',
         },
         logo: {
-            width: '132px',
-            height: '132px',
+            width: '100px',
+            height: '100px',
             margin: '0 auto 10px',
         },
         mainTitle: {
             fontFamily: '"Times New Roman", Times, serif',
-            fontSize: '28pt',
+            fontSize: '32pt',
             fontWeight: 'bold' as 'bold',
             color: '#30475E',
-            margin: '0 0 5px 0',
+            margin: '10px 0 5px 0',
         },
         subtitle: {
-            fontSize: '14pt',
+            fontSize: '16pt',
             color: '#555',
-            margin: '0 0 15px 0',
+            margin: '0 0 20px 0',
             letterSpacing: '1px',
         },
-        divider: {
-            width: '100px',
-            height: '2px',
-            backgroundColor: '#2E8B57',
-            margin: '0 auto 20px',
-        },
         bodyText: {
-            fontSize: '12pt',
+            fontSize: '14pt',
             lineHeight: 1.6,
             color: '#444',
-            marginBottom: '20px',
+            margin: '20px 0',
         },
         studentName: {
             fontFamily: '"Times New Roman", Times, serif',
-            fontSize: '24pt',
+            fontSize: '28pt',
             fontWeight: 'bold' as 'bold',
             color: '#2E8B57',
-            margin: '10px 0',
+            margin: '20px 0',
+            borderBottom: '2px solid #2E8B57',
+            display: 'inline-block' as 'inline-block',
+            paddingBottom: '5px',
         },
         courseName: {
-            fontSize: '14pt',
+            fontSize: '16pt',
             fontWeight: 'bold' as 'bold',
             color: '#30475E',
         },
         signatureContainer: {
+            position: 'absolute' as 'absolute',
+            bottom: '80px',
+            left: '40px',
+            right: '40px',
             display: 'flex',
             justifyContent: 'space-between',
-            alignItems: 'flex-end',
-            width: '100%',
-            marginTop: 'auto',
-            paddingTop: '40px',
-            position: 'relative' as 'relative',
-            zIndex: 2,
         },
         signatureBlock: {
-            width: '45%',
+            width: '200px',
             textAlign: 'center' as 'center',
-            fontSize: '10pt',
+            fontSize: '12pt',
         },
         signatureImage: {
-            height: '68px',
+            height: '50px',
             width: 'auto',
             marginBottom: '5px',
         },
         signatureLine: {
             borderTop: '1px solid #333',
             margin: '0 auto',
-            width: '80%',
+            width: '100%',
         },
         signatureTitle: {
-            marginTop: '5px',
+            marginTop: '8px',
             fontWeight: 'bold' as 'bold',
             color: '#30475E',
         },
-        footer: {
+        footerContainer: {
+            position: 'absolute' as 'absolute',
+            bottom: '40px',
+            left: '40px',
+            right: '40px',
             textAlign: 'center' as 'center',
-            fontSize: '9pt',
+            fontSize: '10pt',
             color: '#777',
-            marginTop: '20px',
-            position: 'relative' as 'relative',
-            zIndex: 2,
-            width: '100%',
-        },
-        certId: {
-            position: 'absolute' as 'absolute',
-            bottom: '-25px',
-            left: '5px',
-            fontSize: '8pt',
-            color: '#999',
-        },
-        issueDate: {
-            position: 'absolute' as 'absolute',
-            bottom: '-25px',
-            right: '5px',
-            fontSize: '8pt',
-            color: '#999',
         }
     };
 
@@ -164,23 +134,22 @@ export default function CertificateTemplate(data: CertificateData) {
             <div style={styles.border}>
                 <img src={data.logoUrl} style={styles.watermark} alt="Watermark" />
                 <div style={styles.content}>
-                    <div>
-                        <img src={data.logoUrl} style={styles.logo} alt="MTech IT Institute Logo" />
-                        <h1 style={styles.mainTitle}>Certificate of Completion</h1>
-                        <h2 style={styles.subtitle}>PROUDLY PRESENTED TO</h2>
-                        <div style={styles.divider}></div>
-                        <p style={styles.bodyText}>This is to certify that</p>
-                        <p style={styles.studentName}>{data.studentName}</p>
-                        <p style={styles.bodyText}>
-                            has successfully completed the course
-                            <br />
-                            <span style={styles.courseName}>{data.testName}</span>
-                            <br />
-                            with a score of <strong>{data.score} / {data.totalMarks} ({data.percentage.toFixed(2)}%)</strong> on {data.examDate}.
-                        </p>
-                    </div>
+                    <img src={data.logoUrl} style={styles.logo} alt="MTech IT Institute Logo" />
+                    <h1 style={styles.mainTitle}>Certificate of Completion</h1>
+                    <h2 style={styles.subtitle}>PROUDLY PRESENTED TO</h2>
+
+                    <p style={styles.bodyText}>This is to certify that</p>
+                    <p style={styles.studentName}>{data.studentName}</p>
+                    <p style={styles.bodyText}>
+                        has successfully completed the course
+                        <br />
+                        <span style={styles.courseName}>{data.testName}</span>
+                        <br />
+                        with a score of <strong>{data.score}/{data.totalMarks} ({data.percentage.toFixed(2)}%)</strong> on {data.examDate}.
+                    </p>
                 </div>
-                 <div style={styles.signatureContainer}>
+
+                <div style={styles.signatureContainer}>
                     <div style={styles.signatureBlock}>
                         <img src={data.directorSignUrl} style={styles.signatureImage} alt="Director's Signature" />
                         <div style={styles.signatureLine}></div>
@@ -192,10 +161,10 @@ export default function CertificateTemplate(data: CertificateData) {
                         <p style={styles.signatureTitle}>Exam Controller</p>
                     </div>
                 </div>
-                 <div style={styles.footer}>
-                    <p>MTech IT Institute | www.mtechitinstitute.in</p>
-                     <p style={styles.certId}>Certificate ID: {data.certificateId}</p>
-                     <p style={styles.issueDate}>Issued on: {data.issueDate}</p>
+
+                <div style={styles.footerContainer}>
+                    <span>Certificate ID: {data.certificateId}</span>
+                    <span style={{ float: 'right' }}>Issued on: {data.issueDate}</span>
                 </div>
             </div>
         </div>
