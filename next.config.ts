4dafa054f439d2eed71e2c2ffb1e9eb63371cb2f
Anoop Patel <anooppbh8@gmail.com>
2025-12-18 14:29:56 +0000

    STEP 1: next.config.js EXACT aisa hi hona chahiye
    👉 Project root me (p

--- next.config.ts
+++ next.config.ts
@@ -10,44 +10,14 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.jdmagicbox.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'jsdl.in',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.ytimg.com',
-        port: '',
-        pathname: '/**',
-      }
-    ],
+    domains: [
+        "i.ytimg.com",
+        "placehold.co",
+        "picsum.photos",
+        "images.jdmagicbox.com",
+        "jsdl.in",
+        "res.cloudinary.com"
+    ]
   },
 };
 
